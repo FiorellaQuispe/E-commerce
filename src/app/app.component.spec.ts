5e75b8797c9fa1e698c5585d0d1c163d8c9646b8
FiorellaQuispe <fiorella_90_17@hotmail.com>
2021-09-08 23:32:14 -0500

    change information

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'M24A'`, () => {
+  it(`should have as title 'Proyect'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('M24A');
+    expect(app.title).toEqual('Proyect');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('M24A app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Proyect app is running!');
   });
 });
